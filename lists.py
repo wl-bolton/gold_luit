6803ef71d58e77ee572deee5174ed7145dc64ff8
wl-bolton <willizoe@gmail.com>
2023-04-11 01:42:44 +0000

    dict

---
+++ lists.py
@@ -0,0 +1,38 @@
+import random
+
+var = []
+
+var2 = [251,386,493]
+
+var3 = [251,386,493,"009"]
+
+print(var3)
+
+var3.append(649)
+
+print(var3)
+#print(dir(var3))
+
+var3.reverse()
+print(var3)
+
+numbers = [1,2,3,4,5]
+print(numbers)
+
+r = range(0,20)
+print(r)
+print(list(r))
+
+for number in numbers:
+    print(number*2)
+    
+for i in range(0,10):
+    print(i)
+    
+print(var3)    
+
+for i in range(len(var3)):
+    print(var3[i])
+    
+    
+    
