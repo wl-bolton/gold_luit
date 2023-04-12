32ac61f8fbac0112a12ffe6b2dd2db0334fd185b
wl-bolton <willizoe@gmail.com>
2023-04-12 05:38:56 +0000

    update c9_dict_lists

--- c9_dict_lists.py
+++ c9_dict_lists.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3.7
 
+#Import module for file information
 import os 
 
+# Create an empty list to store file information
 file_list = [] 
+
+# Get the current working directory
 cwd = os.getcwd()
 
+
+#Create dictionary with file name, size and path
 for file_name in os.listdir(cwd): 
     file_info = {
         "name": file_name,
@@ -12,9 +18,12 @@ for file_name in os.listdir(cwd):
         "path": os.path.join(cwd, file_name)
     }
 
+#Append dictionary info to list
     file_list.append(file_info)
 
+#Loop through file list and print file information
 for file in file_list:
     print("Name:", file["name"])
     print("Size:", file["size"])
     print("Path:", file["path"], "\n")
+    
